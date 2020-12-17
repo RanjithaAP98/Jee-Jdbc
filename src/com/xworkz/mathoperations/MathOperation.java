25776746a5618c972d6b75b5596703544a159a23
Ranjitha A P <71642831+RanjithaAP98@users.noreply.github.com>
2020-12-17 19:07:04 +0530

    Add files via upload

---
+++ src/com/xworkz/mathoperations/MathOperation.java
@@ -0,0 +1,11 @@
+package com.xworkz.mathoperations;
+
+public interface MathOperation {
+	
+	int addition(int a, int b);
+	int subtraction(int a, int b);
+	int multiplication(int a, int b);
+	int division(int a, int b);
+	int modulus(int a, int b);
+
+}
